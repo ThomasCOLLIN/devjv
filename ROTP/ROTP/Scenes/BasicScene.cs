c2eb24049e9dfd15d885f8c24751f9063e95f64c
Thomas COLLIN <thmcollin@gmail.com>
2013-06-02 17:09:43 +0200

    création du menu
    haut et bas + entrer + echap

--- ROTP/ROTP/Scenes/BasicScene.cs
+++ ROTP/ROTP/Scenes/BasicScene.cs
@@ -108,6 +108,5 @@ namespace ROTP.Scenes
 
             }
         }
-
     }
 }
---
+++ ROTP/ROTP/Scenes/Menus/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ROTP.Scenes.Common;
+using ROTP.Scenes.Menus.Common;
+
+namespace ROTP.Scenes.Menus
+{
+    class MainMenu : Menu
+    {
+        public MainMenu(SceneManager manager)
+            : base(manager, "Menu principal")
+        {
+            MenuItem launch = new MenuItem("Lancer le jeu");
+            launch.Selected += OnLaunchSelected;
+            MenuItems.Add(launch);
+
+            MenuItem exit = new MenuItem("Quitter");
+            exit.Selected += OnExitSelected;
+            MenuItems.Add(exit);
+        }
+
+        public override void OnCancel()
+        {
+            SceneManager.Game.Exit();
+        }
+
+        private void OnLaunchSelected(object sender, EventArgs args)
+        {
+            new GameScene(SceneManager).Add();
+        }
+
+        private void OnExitSelected(object sender, EventArgs args)
+        {
+            OnCancel();
+        }
+    }
+}
