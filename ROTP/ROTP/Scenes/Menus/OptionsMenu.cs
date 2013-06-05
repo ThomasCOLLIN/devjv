0a1f1bd02b3b41e02d883145fadc5f4cd1e01c5c
Thomas COLLIN <thmcollin@gmail.com>
2013-06-06 00:14:07 +0200

    ajout option full screen

--- ROTP/ROTP/Scenes/Menus/OptionsMenu.cs
+++ ROTP/ROTP/Scenes/Menus/OptionsMenu.cs
@@ -10,23 +10,33 @@ namespace ROTP.Scenes.Menus
     {
         private Options _options;
 
+        private MenuItem _fs;
+
         public OptionsMenu(SceneManager manager)
             : base(manager, "Options")
         {
             _options = Game1.Options;
 
-            MenuItem fs = new MenuItem("Plein ecran");
-            fs.Selected += OnFullScreenSelected;
-            MenuItems.Add(fs);
+            _fs = new MenuItem("");
+            _fs.Selected += OnFullScreenSelected;
+            MenuItems.Add(_fs);
 
             MenuItem ok = new MenuItem("Valider");
             ok.Selected += OnOkSelected;
             MenuItems.Add(ok);
         }
 
+        public override void Update(GameTime gameTime, bool otherSceneHasFocus, bool coveredByOtherScene)
+        {
+            base.Update(gameTime, otherSceneHasFocus, coveredByOtherScene);
+
+            _fs.Text = "Plein ecran : " + (_options.IsFullScreen? "oui" : "non");
+        }
+
         private void OnFullScreenSelected(object sender, EventArgs args)
         {
             Game1.Graphics.ToggleFullScreen();
+            _options.IsFullScreen = Game1.Graphics.IsFullScreen;
         }
 
         private void OnOkSelected(object sender, EventArgs args)
