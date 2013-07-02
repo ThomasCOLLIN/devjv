3258dcd207e7faa00f048e6bd9ccfd48a41ed7ae
Thomas COLLIN <thmcollin@gmail.com>
2013-07-02 23:54:12 +0200

    win achievement
    useless je sais

---
+++ ROTP/ROTP/Achievements/FiveVictoryAchievement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ROTP.Achievements.Common;
+using ROTP.Save;
+
+namespace ROTP.Achievements
+{
+    class FiveVictoryAchievement : Achievement
+    {
+        private int times;
+
+        public int Times
+        {
+            get { return times; }
+            set { times = value; }
+        }
+
+        public FiveVictoryAchievement()
+            : base("Winner débutant",
+                   "Tu as gagné cinq partie",
+                   "")
+        {
+            SaveManager.OptionsChanged += onEvent;
+        }
+
+        protected override void onEvent(object sender, EventArgs args)
+        {
+            if (!IsOwned)
+            {
+                if (++times >= 5)
+                    IsOwned = true;
+                saveAchievement();
+            }
+        }
+    }
+}
